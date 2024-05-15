5a8e3039a8330982bedefb9874a35d299d3c7bbe
Roche Quejada <rfquejada@up.edu.ph>
2024-05-15 12:23:44 +0800

    fix: fixed landing page after login

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -37,6 +37,7 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         '/': (context) => const LoginPage(),
+        '/login': (context) => const LoginPage(),
         '/donorhomepage': (context) => const DonorHomepage(),
         '/orghomepage': (context) => const OrgHomepage(),
         '/adminhomepage': (context) => const AdminHomepage(),
