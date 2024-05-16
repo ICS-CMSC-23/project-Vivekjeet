2df03b4e284668c69f5f610afe402d6f0743c55a
Roche Quejada <rfquejada@up.edu.ph>
2024-05-16 20:23:48 +0800

    ui: signup page update

---
+++ project/lib/apis/firebase_users_api.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class FirebaseUserAPI {
+  static final FirebaseFirestore db = FirebaseFirestore.instance;
+  static final FirebaseAuth auth = FirebaseAuth.instance;
+
+  // Future<String> addUser(Map<String, dynamic> user) async {
+  //   try {
+  //     final docRef = await db.collection("users").add(user);
+  //     await db.collection("users").doc(docRef.id).update({'id': docRef.id});
+
+  //     return "Successfully added user!";
+  //   } on FirebaseException catch (e) {
+  //     return "Failed with error '${e.code}: ${e.message}";
+  //   }
+  // }
+
+  Stream<QuerySnapshot> getAllUsers() {
+    return db.collection("users").snapshots();
+  }
+
+  // DocumentSnapshot<Map<String, dynamic>>> getUserDetails() {
+  //   return db.collection('user').doc(auth.currentUser?.uid).get();
+  // }
+
+}
---
+++ project/lib/screens/landingpage.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../providers/auth_provider.dart';
+import './login.dart';
+import 'admin_homepage.dart';
+import 'donor_homepage.dart';
+import 'org_homepage.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  String userType = '';
+  @override
+  Widget build(BuildContext context) {
+    Stream<User?> userStream = context.watch<MyAuthProvider>().userStream;
+    
+    return StreamBuilder<User?>(
+      stream: userStream,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("Error: ${snapshot.error}"),
+          );
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (!snapshot.hasData) {
+          return const LoginPage();
+        }
+        return DonorHomepage();
+
+        // final currentUser = context.watch<UsersProvider>().details;
+        // userType = currentUser['type'];
+
+        // User? user = FirebaseAuth.instance.currentUser;
+        // DocumentSnapshot<Map<String, dynamic>> snap = FirebaseFirestore.instance.collection('Users').doc(user?.uid).get() as DocumentSnapshot<Map<String, dynamic>>;
+        // userType = snap['type'];
+        
+
+        // switch (userType) {
+        //   case 'Donor':
+        //     return const DonorHomepage();
+        //   case 'Organization':
+        //     return const OrgHomepage();
+        //   case 'Admin':
+        //     return const AdminHomepage();
+        //   default:
+        //     return const LoginPage()
+        // }
+      },
+    );
+  }
+}
