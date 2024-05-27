cca1f7d6f8b686ef352052cfb15a57006c971dd4
Roche Quejada <rfquejada@up.edu.ph>
2024-05-27 14:56:13 +0800

    added donation drives api and providers

--- project/lib/apis/firebase_drive_api.dart
+++ project/lib/apis/firebase_drive_api.dart
@@ -6,12 +6,31 @@ class FirebaseDriveAPI {
   static final FirebaseFirestore db = FirebaseFirestore.instance;
   static final FirebaseAuth auth = FirebaseAuth.instance;
 
-  // Stream<QuerySnapshot> getDrives(String organizationId) {
-  //   return db
-  //       .collection('donationDrives')
-  //       .where('organization', isEqualTo: db.doc('users/$organizationId'))
-  //       .snapshots();
-  // }
+  Stream<QuerySnapshot> getDrivesOfOrganization(String organizationId) {
+    return db
+        .collection('donationDrives')
+        .where('organization', isEqualTo: db.doc('users/$organizationId'))
+        .snapshots();
+  }
+
+  Future<List<DocumentReference>> getDonationsOfDrive(String driveId) async {
+    try {
+      DocumentSnapshot driveDoc = await db.collection('donationDrives').doc(driveId).get();
+      if (driveDoc.exists) {
+        List<DocumentReference> donations = List<DocumentReference>.from(driveDoc['donations']);
+        return donations;
+      } else {
+        throw Exception("Drive not found");
+      }
+    } catch (e) {
+      print("Failed to get donation references: $e");
+      return [];
+    }
+  }
+  
+  Future<DocumentSnapshot> getDriveById(String driveId) async {
+    return await db.collection('donationDrives').doc(driveId).get();
+  }
 
   Future<String> addDrive(Map<String, dynamic> drive) async {
     try {
@@ -35,6 +54,28 @@ class FirebaseDriveAPI {
         await db.collection('donationDrives').doc(driveId).delete();
 
         return "Successfully deleted!";
+      } else {
+        return "Drive not found!";
+      }
+    } catch (e) {
+      return "Failed with error: $e";
+    }
+  }
+
+  Future<String> editDrive(String? driveId, String newDriveName, String newDescription, List<String> newPhotos) async {
+    try {
+      QuerySnapshot querySnapshot = await db
+          .collection('donationDrives')
+          .where('driveId', isEqualTo: driveId)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+
+        await db.collection('friends').doc(driveId).update({"driveName": newDriveName});
+        await db.collection('friends').doc(driveId).update({"description": newDescription});
+        await db.collection('friends').doc(driveId).update({"photos": newPhotos});
+
+        return "Successfully edited!";
       } else {
         return "Drive not found!";
       }
--- project/lib/providers/drive_provider.dart
+++ project/lib/providers/drive_provider.dart
@@ -1,22 +1,37 @@
 // import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:project/models/drive_model.dart';
 import '../apis/firebase_drive_api.dart';
 
 class DriveProvider with ChangeNotifier {
   late FirebaseDriveAPI firebaseService;
-  // late Stream<QuerySnapshot> _drivesStream;
+  late Stream<QuerySnapshot> _drivesStream;
+  List<DocumentReference> _donationsOfDrive = [];
+  late DocumentSnapshot _selectedDriveStream;
 
   DriveProvider() {
     firebaseService = FirebaseDriveAPI();
   }
 
-  // Stream<QuerySnapshot> get drives => _drivesStream;
+  Stream<QuerySnapshot> get drives => _drivesStream;
+  List<DocumentReference> get donationReferences => _donationsOfDrive;
+  DocumentSnapshot<Object?> get selectedDrive => _selectedDriveStream;
 
-  // void loadDrives(String organizationId) {
-  //   _drivesStream = firebaseService.getDrives(organizationId);
-  //   notifyListeners();
-  // }
+  void loadDrivesOfOrganization(String organizationId) {
+    _drivesStream = firebaseService.getDrivesOfOrganization(organizationId);
+    notifyListeners();
+  }
+
+  Future<void> loadDonationsOfDrive(String driveId) async {
+    _donationsOfDrive = await firebaseService.getDonationsOfDrive(driveId);
+    notifyListeners();
+  }
+
+  void fetchDriveById(String driveId) async {
+    _selectedDriveStream = await firebaseService.getDriveById(driveId);
+    notifyListeners();
+  }
 
   void addDrive(DriveModel drive) async {
     String message = await firebaseService.addDrive(drive.toJson(drive));
@@ -26,6 +41,12 @@ class DriveProvider with ChangeNotifier {
 
   void deleteDrive(String? driveId) async {
     String message = await firebaseService.deleteDonation(driveId);
+    print(message);
+    notifyListeners();
+  }
+
+  void editDrive(String driveId, String newDriveName, String newDescription, List<String> newPhotos) async {
+    String message = await firebaseService.editDrive(driveId, newDriveName, newDescription, newPhotos);
     print(message);
     notifyListeners();
   }
